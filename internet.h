9cae47be22c19bcd125ead30ee41db263e858a0f
BalnerEli <102267619+BalnerEli@users.noreply.github.com>
2022-06-18 12:13:34 -0500

    Update internet.h

--- internet.h
+++ internet.h
@@ -10,17 +10,47 @@
 
 #include "operator.h"
 
+//Clase InternetOperator, deriva de Operator
 class InternetOperator : public Operator {
-private:
-  const double LIMITGB = 1.0;
 
-public:
-  InternetOperator(int, double, double, double, int, OperatorType);
-  InternetOperator(const InternetOperator&);
+	//Atributos
+	private:
+		
+		const double LIMITGB = 10.0;
 
-  double calculateTalkingCost(int, int);
-  double calculateMessageCost(int, int, int);
-  double calculateNetworkCost(double);
+	//Metodos
+	public:
+		
+		//Constructorees
+		InternetOperator(int, double, double, double, int, OperatorType);
+		InternetOperator(const InternetOperator&);
+
+		//Calculamos
+		double calculateTalkingCost(int, int);
+		double calculateMessageCost(int, int, int);
+		double calculateNetworkCost(double);
 };
+
+InternetOperator::InternetOperator(int i, double t, double mC, double nC, int dR, OperatorType ty)
+	: Operator(i, t, mC, nC, dR, ty) {
+
+}
+
+InternetOperator::InternetOperator(const InternetOperator& other)
+	: Operator(other) {
+
+}
+
+double InternetOperator::calculateTalkingCost(int minute, int age) {
+
+}
+
+double InternetOperator:: calculateMessageCost(int quantity, int thisOpId, int otherOpId) {
+
+}
+
+double InternetOperator::calculateNetworkCost(double amount) {
+
+}
 
 #endif
