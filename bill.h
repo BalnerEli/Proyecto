2a91e2b0177731ab53cd9db56774e92ba7513ca4
BalnerEli <102267619+BalnerEli@users.noreply.github.com>
2022-06-18 13:24:12 -0500

    Update bill.h

--- bill.h
+++ bill.h
@@ -1,4 +1,4 @@
-// =========================================================
+=========================================================
 // File: bill.h
 // Author: 
 // Date: 18/06/2022
@@ -8,81 +8,125 @@
 #ifndef BILL_H
 #define BILL_H
 
-
+//Clase Bill
 class Bill {
-private:
-  double limitAmount, currentDebt, totalMoneySpent;
+    
+    //Atributod
+    private:
+        
+        double limitAmount, currentDebt, totalMoneySpent;
 
-public:
-  Bill();
-  Bill(double);
-  Bill(const Bill&);
+    //Metodos
+    public:
+       
+        //Constructores
+        Bill();
+        Bill(double);
+        Bill(const Bill&);
 
-  double getLimitAmount() const;
-  double getCurrentDebt() const;
-  double getTotalMoneySpent() const;
+        //Obtener valores
+        double getLimitAmount() const;
+        double getCurrentDebt() const;
+        double getTotalMoneySpent() const;
 
-  void add(double);
-  void pay(double);
-  void changeTheLimit(double);
-  bool check(double) const;
+        //Metodos de operaciones
+        double add(double);
+        double pay(double);
+        double changeTheLimit(double);
+        bool check(double) const;
 };
 
 Bill::Bill() {
+    
     limitAmount = 0;
     currentDebt = 0;
     totalMoneySpent = 0;
 }
 
-// check Bill method
 Bill::Bill(double limit) {
+    
     if (limit <= 0) {
+    
         limit = 0;
     }
+
+    
     limitAmount = limit;
     currentDebt = 0;
     totalMoneySpent = 0;
 }
 
 Bill::Bill(const Bill& other) {
+    
     limitAmount = other.limitAmount;
     currentDebt = other.currentDebt;
     totalMoneySpent = other.totalMoneySpent;
 }
 
 double Bill::getLimitAmount() const {
+    
     return limitAmount;
 }
 
 double Bill::getCurrentDebt() const {
+    
     return currentDebt;
 }
 
 double Bill::getTotalMoneySpent() const {
+    
     return totalMoneySpent;
 }
 
-void Bill::add(double amount) {
+double Bill::add(double amount) {
+    
     if (amount <= 0) {
+        
         return currentDebt;
-    }
-    else return(currentDebt += amount);
-}
+    
+    }else {
 
-void Bill::pay(double amount) {
-    if (amount <= 0) {
-        return currentDebt;
-     }
-    else return (currentDebt -= amount);
-}
+        return currentDebt += amount;
 
-void Bill::changeTheLimit(double amount) {
-    if (limit > 0 && amount >= currentDebt) {
-        limitAmount = amount;
     }
 }
+
+double Bill::pay(double amount) {
+    
+    if (amount <= 0) {
+    
+        return currentDebt;
+    }
+    else {
+
+        return currentDebt -= amount;
+
+    }
+
+}
+
+double Bill::changeTheLimit(double amount) {
+
+    if (amount <= 0) {
+
+        return currentDebt;
+
+    }else {
+
+        if (limitAmount > currentDebt) {
+
+            limitAmount += amount;
+
+        }
+    }
+
+}
+
 
 bool Bill::check(double amount) const {
-    return (amount + currentDebt ) <= limitAmount;
+
+    return currentDebt + amount;
+
 }
+
 #endif
