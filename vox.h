df3fdb3411aa17192ba681791d637cb4d6d09030
BalnerEli <102267619+BalnerEli@users.noreply.github.com>
2022-06-18 15:09:44 -0500

    Update vox.h

--- vox.h
+++ vox.h
@@ -10,43 +10,55 @@
 
 #include "operator.h"
 
-//Clase VoxOperator, deriva de la clase Operatoe
 class VoxOperator : public Operator {
+public:
+  VoxOperator(int, double, double, double, int, OperatorType);
+  VoxOperator(const VoxOperator&);
 
-	//Metodos
-	public:
-		
-		//Constructorees
-		VoxOperator(){}
-		VoxOperator(int, double, double, double, double, OperatorType);
-		VoxOperator(const VoxOperator&);
-
-		//Calculamos 
-		double calculateTalkingCost(int, int);
-		double calculateMessageCost(int, int, int);
-		double calculateNetworkCost(double);
+  double calculateTalkingCost(int, int);
+  double calculateMessageCost(int, int, int);
+  double calculateNetworkCost(double);
 };
 
-VoxOperator::VoxOperator(int id, double discountRate, double talkingCharge, double messageCost, double networkCharge, OperatorType type): 
-	Operator(id, discountRate, talkingCharge, messageCost, networkCharge, type) {
+VoxOperator::VoxOperator(int i, double t, double mC, double nC, int dR, OperatorType ty)
+	: Operator(i, t, mC, nC, dR, ty, VOXOPERATOR) {
 
 }
 
 VoxOperator::VoxOperator(const VoxOperator& other)
-	: Operator(other) {
+	: Operator(other.id, other.talkingCharge, other.networkCharge, other.discountRate, other.type, other.totalSpentTalkingTime, other.totalMessageSent, other.totalInternetUsage) {
 
 }
 
 double VoxOperator::calculateTalkingCost(int minute, int age) {
-
+	double cost = 0;
+	if (minute < 0 && age < 0) {
+		cost = minute*talkingCharge;
+		if (age < 18 || age > 65) {
+			cost = cost*(cost-discountRate/100);
+		}
+	}
+	return cost;
 }
 
-double VoxOperator::calculateMessageCost(int quantity, int thisOpId, int otherOpId) {
-
+double VoxOperator::double calculateMessageCost(int quantity, int thisOpId, int otherOpId) {
+	double cost = 0;
+	if (quantity < 0) {
+		cost = quantity*messageCost;
+		if (thisOpId == otherOpId) {
+			cost=cost*(cost-discountRate/100);
+		}
+	}
+	return cost;
+}
 }
 
-double VoxOperator::calculateNetworkCost(double amount) {
-
+void VoxOperator::calculateNetworkCost(double amount) {
+	double cost = 0;
+	if (amount > 0) {
+		cost = amount*networkCharge;
+	}
+	return cost;
 }
 
 #endif
