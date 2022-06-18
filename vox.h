22d1aa7d1cae0344e5770621376c400984e03a0d
BalnerEli <102267619+BalnerEli@users.noreply.github.com>
2022-06-18 12:14:11 -0500

    Update vox.h

--- vox.h
+++ vox.h
@@ -1,3 +1,5 @@
+#pragma once
+
 // =========================================================
 // File: vox.h
 // Author:
@@ -10,14 +12,43 @@
 
 #include "operator.h"
 
+//Clase VoxOperator, deriva de la clase Operatoe
 class VoxOperator : public Operator {
-public:
-  VoxOperator(int, double, double, double, int, OperatorType);
-  VoxOperator(const VoxOperator&);
 
-  double calculateTalkingCost(int, int);
-  double calculateMessageCost(int, int, int);
-  double calculateNetworkCost(double);
+	//Metodos
+	public:
+		
+		//Constructorees
+		VoxOperator(){}
+		VoxOperator(int, double, double, double, double, OperatorType);
+		VoxOperator(const VoxOperator&);
+
+		//Calculamos 
+		double calculateTalkingCost(int, int);
+		double calculateMessageCost(int, int, int);
+		double calculateNetworkCost(double);
 };
+
+VoxOperator::VoxOperator(int id, double discountRate, double talkingCharge, double messageCost, double networkCharge, OperatorType type): 
+	Operator(id, discountRate, talkingCharge, messageCost, networkCharge, type) {
+
+}
+
+VoxOperator::VoxOperator(const VoxOperator& other)
+	: Operator(other) {
+
+}
+
+double VoxOperator::calculateTalkingCost(int minute, int age) {
+
+}
+
+double VoxOperator::calculateMessageCost(int quantity, int thisOpId, int otherOpId) {
+
+}
+
+double VoxOperator::calculateNetworkCost(double amount) {
+
+}
 
 #endif
