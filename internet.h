fb7ecbfa57af30ca2586bbb63e248473dffe3eb0
BalnerEli <102267619+BalnerEli@users.noreply.github.com>
2022-06-18 15:10:17 -0500

    Update internet.h

--- internet.h
+++ internet.h
@@ -1,4 +1,4 @@
-=========================================================
+// =========================================================
 // File: vox.h
 // Author:
 // Date:
@@ -10,47 +10,59 @@
 
 #include "operator.h"
 
-//Clase InternetOperator, deriva de Operator
 class InternetOperator : public Operator {
+private:
+  const double LIMITGB = 10.0;
 
-	//Atributos
-	private:
-		
-		const double LIMITGB = 10.0;
+public:
+  InternetOperator(int, double, double, double, int, OperatorType);
+  InternetOperator(const InternetOperator&);
 
-	//Metodos
-	public:
-		
-		//Constructorees
-		InternetOperator(int, double, double, double, int, OperatorType);
-		InternetOperator(const InternetOperator&);
-
-		//Calculamos
-		double calculateTalkingCost(int, int);
-		double calculateMessageCost(int, int, int);
-		double calculateNetworkCost(double);
+  double calculateTalkingCost(int, int);
+  double calculateMessageCost(int, int, int);
+  double calculateNetworkCost(double);
 };
 
 InternetOperator::InternetOperator(int i, double t, double mC, double nC, int dR, OperatorType ty)
-	: Operator(i, t, mC, nC, dR, ty) {
+	: Operator(i, t, mC, nC, dR, ty, INTERNETOPERATOR) {
 
 }
 
 InternetOperator::InternetOperator(const InternetOperator& other)
-	: Operator(other) {
+	: Operator(other.id, other.talkingCharge, other.networkCharge, other.discountRate, other.type, other.totalSpentTalkingTime, other.totalMessageSent, other.totalInternetUsage) {
 
 }
 
 double InternetOperator::calculateTalkingCost(int minute, int age) {
-
+    double cost = 0;
+    if (minute < 0 && age < 0) {
+        cost = minute * talkingCharge;
+        if (minute < 2) {
+            cost= cost * (cost-(discountRate / 100));
+        }
+    }
+    return cost;
 }
 
-double InternetOperator:: calculateMessageCost(int quantity, int thisOpId, int otherOpId) {
-
+double InternetOperator::double calculateMessageCost(int quantity, int thisOpId, int otherOpId) {
+    double cost = 0;
+    if (quantity < 0) {
+        cost = quantity*messageCost;
+        if (quantity < 3) {
+            cost = cost*(cost(discountRate/100));
+        }   
+    }
+    return cost;
 }
-
 double InternetOperator::calculateNetworkCost(double amount) {
-
+    double cost = 0;
+    if (amount+totalInternetUsage > LIMITGB) {
+        cost = (amount-LIMITGB)*networkCharge;
+    }
+    else {
+        cost=0
+    }
+    return cost;
 }
 
 #endif
