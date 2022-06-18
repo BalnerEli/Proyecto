93d77db3dacc87b91a15a3eddc1ad75a7e3ab0ca
BalnerEli <102267619+BalnerEli@users.noreply.github.com>
2022-06-18 16:39:05 -0500

    Update internet.h

--- internet.h
+++ internet.h
@@ -1,5 +1,37 @@
-// =========================================================
+=========================================================
 // File: internet.h
+// Author:
+// Date:
+// Description:
+// =========================================================
+
+#ifndef VOXOPERATOR_H
+#define VOXOPERATOR_H
+
+#include "operator.h"
+
+//Clase VoxOperator, deriva de la clase Operatoe
+class VoxOperator : public Operator {
+
+	//Metodos
+	public:
+		
+		//Constructorees
+		VoxOperator(){}
+		VoxOperator(int, double, double, double, double, OperatorType);
+		VoxOperator(const VoxOperator&);
+
+		//Calculamos 
+		double calculateTalkingCost(int, int);
+		double calculateMessageCost(int, int, int);
+		double calculateNetworkCost(double);
+};
+
+VoxOperator::VoxOperator(int id, double discountRate, double talkingCharge, double messageCost, double networkCharge…
+[4:09 p. m., 18/6/2022] Gonzalo Chavez: VoxOperator
+[4:09 p. m., 18/6/2022] Gonzalo Chavez: #pragma once
+// =========================================================
+// File: vox.h
 // Author:
 // Date:
 // Description:
@@ -10,59 +42,86 @@
 
 #include "operator.h"
 
+//Clase InternetOperator, deriva de Operator
 class InternetOperator : public Operator {
-private:
-  const double LIMITGB = 10.0;
 
-public:
-  InternetOperator(int, double, double, double, int, OperatorType);
-  InternetOperator(const InternetOperator&);
+	//Atributos
+	private:
+		
+		const double LIMITGB = 10.0;
 
-  double calculateTalkingCost(int, int);
-  double calculateMessageCost(int, int, int);
-  double calculateNetworkCost(double);
+	//Metodos
+	public:
+		
+		//Constructorees
+		InternetOperator(int, double, double, double, double, OperatorType);
+		InternetOperator(const InternetOperator&);
+
+		//Calculamos
+		double calculateTalkingCost(int, int);
+		double calculateMessageCost(int, int, int);
+		double calculateNetworkCost(double);
 };
 
-InternetOperator::InternetOperator(int i, double t, double mC, double nC, int dR, OperatorType ty)
-	: Operator(i, t, mC, nC, dR, ty, INTERNETOPERATOR) {
+InternetOperator::InternetOperator(int id, double discountRate, double talkingCharge, double messageCost, double networkCharge, OperatorType type)
+	: Operator(id, discountRate, talkingCharge, messageCost, networkCharge, type) {
 
 }
 
 InternetOperator::InternetOperator(const InternetOperator& other)
-	: Operator(other.id, other.talkingCharge, other.networkCharge, other.discountRate, other.type, other.totalSpentTalkingTime, other.totalMessageSent, other.totalInternetUsage) {
+	: Operator(other) {
 
 }
 
 double InternetOperator::calculateTalkingCost(int minute, int age) {
-    double cost = 0;
-    if (minute < 0 && age < 0) {
-        cost = minute * talkingCharge;
-        if (minute < 2) {
-            cost= cost * (cost-(discountRate / 100));
-        }
-    }
-    return cost;
+
+	if (minute < 0 and age < 0) {
+
+		return 0.0;
+
+	}else if (minute > 0 and minute < 3) {
+
+		return (talkingCharge * minute) - ((talkingCharge * minute) * discountRate);
+
+	}else {
+
+		return talkingCharge * minute;
+
+	}
+
 }
 
-double InternetOperator::double calculateMessageCost(int quantity, int thisOpId, int otherOpId) {
-    double cost = 0;
-    if (quantity < 0) {
-        cost = quantity*messageCost;
-        if (quantity < 3) {
-            cost = cost*(cost(discountRate/100));
-        }   
-    }
-    return cost;
+double InternetOperator:: calculateMessageCost(int quantity, int thisOpId, int otherOpId) {
+
+	if (quantity < 0) {
+
+		return 0.0;
+
+	}else if(quantity > 0 and quantity < 4) {
+
+		return (messageCost * quantity) - ((messageCost * quantity) * discountRate);
+
+	}else {
+
+		return messageCost * quantity;
+	}
+
 }
+
 double InternetOperator::calculateNetworkCost(double amount) {
-    double cost = 0;
-    if (amount+totalInternetUsage > LIMITGB) {
-        cost = (amount-LIMITGB)*networkCharge;
-    }
-    else {
-        cost=0
-    }
-    return cost;
-}
 
+	if (totalInternetUsage > 0) {
+
+		amount = totalInternetUsage * networkCharge;
+
+	}
+	else {
+
+		amount  = 0.0;
+
+	}
+
+	return amount;
+}
+	
 #endif
